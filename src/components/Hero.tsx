7c2e6014780b1640edbc70034065d298fd5ae46d
Gabriel <estudo.gabmartts@gmail.com>
2022-12-14 22:12:36 -0300

    Type from Props Api fetch

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,6 +2,14 @@ import useSWR from "swr"
 import Image from "./Image"
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
+interface ApiObj {
+    name: string;
+    category: string;
+    image: string;
+    logo: string;
+
+}
+
 export default function Hero (){
     const { data, error, isLoading } = useSWR('https://api.brchallenges.com/api/blizzard/games', fetcher)
     if (error) return <div>falhou ao carregar</div>
@@ -9,7 +17,7 @@ export default function Hero (){
     return(
         <div className="w-screen flex justify-center bg-black">
         <div className="flex flex-row flex-wrap w-4/5 justify-center gap-10 p-2">
-        {data.map( e => <Image {...e} key={e.name} />)}
+        {data.map( (e: ApiObj) => <Image {...e} key={e.name} />)}
         <div className="w-72 text-white flex flex-col justify-center items-center border-2 border-[#212428] rounded-md">
             <h1>Blizzard</h1>
             <p>Ver todos os Jogos</p>
