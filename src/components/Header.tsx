476d67119e1db21aeeca22bac676c78463d7356f
Gabriel <estudo.gabmartts@gmail.com>
2023-01-13 16:38:35 -0300

    Links now in style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,12 @@ import { useState } from "react";
 import Menu from "./Menu";
 
 export default function Header (){
-    const [defineImg, setDefineImg] = useState('main-image')
+    const [defineImg, setDefineImg] = useState("url(https://res.cloudinary.com/ddsivmxle/image/upload/v1671069014/image_2_qnmfvt.webp)")
     return(
-        <div className={`bg-${defineImg} h-[640px]`}>
+        <div className={`h-[640px]`} style={{backgroundImage: defineImg}}>
             <Menu/>
-            <img className={`bg-${defineImg} w-80 h-40`} alt="" />
-            <button className="w-10 h-5 bg-slate-300 rounded-sm" onClick={() => setDefineImg('footer-image')}>Teste</button>
-            <button className="w-10 h-5 bg-slate-300 rounded-sm" onClick={() => setDefineImg('second-image')}>Teste</button>
+            <button className="w-10 h-5 bg-slate-300 rounded-sm" onClick={() => setDefineImg('url(https://res.cloudinary.com/ddsivmxle/image/upload/v1671069014/image_2_qnmfvt.webp)')}>Teste</button>
+            <button className="w-10 h-5 bg-slate-300 rounded-sm" onClick={() => setDefineImg('url(https://res.cloudinary.com/ddsivmxle/image/upload/c_scale,w_1920/v1673631871/hearthstone-bg_aa2nay.png)')}>Teste</button>
             <button className="w-10 h-5 bg-slate-300 rounded-sm" onClick={() => setDefineImg('another-image')}>Teste</button>
             <button className="w-10 h-5 bg-slate-300 rounded-sm" onClick={() => setDefineImg('footer-image')}>Teste</button>
 
