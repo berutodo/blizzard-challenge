5b9b40be175c0aa06e43024ee1ff90f74c26cfe5
Gabriel <estudo.gabmartts@gmail.com>
2023-01-13 22:45:38 -0300

    Added some images and solved the problem with the main Header Image switching

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,13 +4,27 @@ import Menu from "./Menu";
 export default function Header (){
     const [defineImg, setDefineImg] = useState("url(https://res.cloudinary.com/ddsivmxle/image/upload/v1671069014/image_2_qnmfvt.webp)")
     return(
-        <div className={`h-[640px]`} style={{backgroundImage: defineImg}}>
+        <div className={`h-[640px] bg-center`} style={{backgroundImage: defineImg}}>
             <Menu/>
-            <button className="w-10 h-5 bg-slate-300 rounded-sm" onClick={() => setDefineImg('url(https://res.cloudinary.com/ddsivmxle/image/upload/v1671069014/image_2_qnmfvt.webp)')}>Teste</button>
-            <button className="w-10 h-5 bg-slate-300 rounded-sm" onClick={() => setDefineImg('url(https://res.cloudinary.com/ddsivmxle/image/upload/c_scale,w_1920/v1673631871/hearthstone-bg_aa2nay.png)')}>Teste</button>
-            <button className="w-10 h-5 bg-slate-300 rounded-sm" onClick={() => setDefineImg('another-image')}>Teste</button>
-            <button className="w-10 h-5 bg-slate-300 rounded-sm" onClick={() => setDefineImg('footer-image')}>Teste</button>
+            <div className="flex sm:flex-col sm:gap-8 gap-4">
+            <button className="w-16 h-16 rounded-sm" onClick={() => setDefineImg('url(https://res.cloudinary.com/ddsivmxle/image/upload/v1671069014/image_2_qnmfvt.webp)')}>
+                <img src="/game-1.png" alt="" />
+            </button>
+            <button className="w-16 h-16  rounded-sm" onClick={() => setDefineImg('url(https://res.cloudinary.com/ddsivmxle/image/upload/c_scale,w_1920/v1673631871/hearthstone-bg_aa2nay.png)')}>
+            <img src="/game-2.png" alt="" />
 
+            </button>
+            <button className="w-16 h-16  rounded-sm" onClick={() => setDefineImg('another-image')}>
+            <img src="/game-3.png" alt="" />
+            </button>
+            <button className="w-16 h-16 rounded-sm" onClick={() => setDefineImg('footer-image')}>
+            <img src="/game-4.png" alt="" />
+            </button>
+            <button className="w-16 h-16  rounded-sm" onClick={() => setDefineImg('footer-image')}>
+            <img src="/game-5.png" alt="" />
+            </button>
+            </div>
+            
         </div>
     )
 
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,7 +1,7 @@
 import blizzardSvg from '../../src/assets/blizzard.svg'
 export default function Menu () {
     return (
-        <div className="flex border-b-2 border-opacity-10 p-6">
+        <div className="flex border-b-[0.1px] border-opacity-10 p-6">
             <img src={blizzardSvg} alt="" />
         </div>
     )
