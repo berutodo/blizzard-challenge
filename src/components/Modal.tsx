8f3a16b1b2a8a7f53e6cdd1a56a4643dce0d14ea
Gabriel <estudo.gabmartts@gmail.com>
2023-02-10 22:25:18 -0300

    modal

---
+++ src/components/Modal.tsx
@@ -0,0 +1,18 @@
+export default function Modal (){
+    return (
+        <div>
+            <form action="">
+                <input type="text" placeholder="E-mail ou telefone" />
+                <input type="password" placeholder="Senha"  />
+                <button type="submit">Conectar-se</button>
+            </form>
+            <h1>Ou conecte-se com</h1>
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+
+            <h1><a href="">Criar uma conta</a>Battle.net de graça</h1>
+            <a href="">Não consegue logar?</a>
+        </div>
+    )
+}
